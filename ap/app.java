34e8371cb3c135fda45a435afafd529498b844f7
sucerquia22 <juandavidariassucerquia@gmail.com>
2026-03-07 00:15:28 -0500

    inicioo

--- ap/app.java
+++ ap/app.java
@@ -3,6 +3,8 @@ import libreria.lib;
 public class app {
     public static void main(String[] args) {
         System.out.println(lib.sumar(5, 3));
+        System.out.println(lib.sumar(5, 3));
+
     }
 
 }
